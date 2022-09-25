ce7d961751601df2645d46ab840273557779ded5
psmacabulos <patrick.macabulos@gmail.com>
2022-09-25 18:23:25 +0800

    Solved exercise 04 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function (arr, ...element) {
+  // loop over the array and remove the element/s that was on the argument
+  for (let el of element) {
+    let index = arr.findIndex((value) => el === value)
+    if (index < 0) {
+      continue
+    } else {
+      arr.splice(index, 1)
+    }
+  }
+  return arr
+}
 
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray
